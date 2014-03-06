5599d57398a843a6e43ae4504b12251440e14463
MaryHal <maryhal@users.noreply.github.com>
2014-03-06 15:06:25 -0800

    Abstracted Firework Launcher from TestScreen

--- Burst-core/src/fp/infiniteset/Burst/FireworkLauncher.java
+++ Burst-core/src/fp/infiniteset/Burst/FireworkLauncher.java
@@ -1,6 +1,82 @@
 package fp.infiniteset.Burst;
 
+import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Random;
+
 public class FireworkLauncher
 {
-    
+    private ParticleEffect prototype;
+    private ParticleEffectPool effectPool;
+    private Array<PooledEffect> effects;
+
+    private FireworkPool fireworkPool;
+    private Array<Firework> fireworks;
+
+    private float[][] fireworkColors;
+    private Random rng;
+
+    public FireworkLauncher()
+    {
+        prototype = new ParticleEffect();
+        prototype.load(Gdx.files.internal("effects/test.p"), Gdx.files.internal("effects"));
+        prototype.start();
+
+        effectPool = new ParticleEffectPool(prototype, 0, 70);
+        effects = new Array<PooledEffect>();
+
+        fireworkPool = new FireworkPool(64, 128);
+        fireworks = new Array<Firework>();
+
+        fireworkColors = new float[][] {
+            {1.0f, 0.3f, 0.3f, 1.0f},
+            {0.3f, 1.0f, 0.3f, 1.0f},
+            {0.3f, 0.3f, 1.0f, 1.0f},
+
+            {0.3f, 1.0f, 1.0f, 1.0f},
+            {1.0f, 0.3f, 1.0f, 1.0f},
+            {1.0f, 1.0f, 0.3f, 1.0f},
+        };
+
+        rng = new Random();
+    }
+
+    public void dispose()
+    {
+        prototype.dispose();
+    }
+
+    public void fire(Vector2 position)
+    {
+        PooledEffect effect = effectPool.obtain();
+        float[] color = fireworkColors[rng.nextInt(fireworkColors.length)];
+        effect.getEmitters().peek().getTint().setColors(color);
+        effect.setPosition(position.x, position.y);
+        effects.add(effect);
+    }
+
+    public void update()
+    {
+    }
+
+    public void draw(SpriteBatch batch, float delta)
+    {
+        for (PooledEffect effect : effects)
+        {
+            effect.draw(batch, delta);
+            if(effect.isComplete())
+            {
+                effects.removeValue(effect, true);
+                effect.free();
+            }
+        }
+    }
 }
--- Burst-core/src/fp/infiniteset/Burst/TestScreen.java
+++ Burst-core/src/fp/infiniteset/Burst/TestScreen.java
@@ -1,21 +1,16 @@
 package fp.infiniteset.Burst;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+/* import com.badlogic.gdx.audio.*; */
 
-import com.badlogic.gdx.audio.*;
-
-import com.badlogic.gdx.utils.Array;
-
-import java.util.Random;
+import com.badlogic.gdx.math.Vector2;
 
 public class TestScreen implements Screen
 {
@@ -24,15 +19,7 @@ public class TestScreen implements Screen
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
-    private ParticleEffect prototype;
-    private ParticleEffectPool effectPool;
-    private Array<PooledEffect> effects;
-
-    private FireworkPool fireworkPool;
-    private Array<Firework> fireworks;
-
-    private float[][] fireworkColors;
-    private Random rng;
+    private FireworkLauncher launcher;
 
     /* private Music music; */
 
@@ -52,26 +39,18 @@ public class TestScreen implements Screen
         /* f.update(delta); */
         /* f.draw(camera); */
 
-        if (rng.nextInt(20) == 0)
-        {
-            PooledEffect effect = effectPool.obtain();
-            float[] color = fireworkColors[rng.nextInt(fireworkColors.length)];
-            effect.getEmitters().peek().getTint().setColors(color);
-            effect.setPosition(rng.nextInt(300) + 90, rng.nextInt(200) + 50);
-            effects.add(effect);
-        }
+        /* if (rng.nextInt(20) == 0) */
+        /* { */
+        /*     PooledEffect effect = effectPool.obtain(); */
+        /*     float[] color = fireworkColors[rng.nextInt(fireworkColors.length)]; */
+        /*     effect.getEmitters().peek().getTint().setColors(color); */
+        /*     effect.setPosition(rng.nextInt(300) + 90, rng.nextInt(200) + 50); */
+        /*     effects.add(effect); */
+        /* } */
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        for (PooledEffect effect : effects)
-        {
-            effect.draw(batch, delta);
-            if(effect.isComplete())
-            {
-                effects.removeValue(effect, true);
-                effect.free();
-            }
-        }
+        launcher.draw(batch, delta);
         batch.end();
     }
 
@@ -92,27 +71,7 @@ public class TestScreen implements Screen
         batch = new SpriteBatch(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.setProjectionMatrix(camera.combined);
 
-        prototype = new ParticleEffect();
-        prototype.load(Gdx.files.internal("effects/test.p"), Gdx.files.internal("effects"));
-        prototype.start();
-
-        effectPool = new ParticleEffectPool(prototype, 0, 70);
-        effects = new Array<PooledEffect>();
-
-        fireworkPool = new FireworkPool(64, 128);
-        fireworks = new Array<Firework>();
-
-        fireworkColors = new float[][] {
-            {1.0f, 0.3f, 0.3f, 1.0f},
-            {0.3f, 1.0f, 0.3f, 1.0f},
-            {0.3f, 0.3f, 1.0f, 1.0f},
-
-            {0.3f, 1.0f, 1.0f, 1.0f},
-            {1.0f, 0.3f, 1.0f, 1.0f},
-            {1.0f, 1.0f, 0.3f, 1.0f},
-        };
-
-        rng = new Random();
+        launcher = new FireworkLauncher();
 
         /* music = Gdx.audio.newMusic(Gdx.files.internal("music/Melodica.mp3")); */
 
@@ -122,10 +81,8 @@ public class TestScreen implements Screen
             {
                 if (keycode == Keys.A)
                 {
-                    PooledEffect effect = effectPool.obtain();
-                    effect.setPosition(Gdx.graphics.getWidth()  / 2,
-                                       Gdx.graphics.getHeight() / 2);
-                    effects.add(effect);
+                    launcher.fire(new Vector2(Gdx.graphics.getWidth()  / 2,
+                                              Gdx.graphics.getHeight() / 2));
 
                     return true;
                 }
@@ -134,12 +91,7 @@ public class TestScreen implements Screen
 
             public boolean touchUp(int x, int y, int pointer, int button)
             {
-                PooledEffect effect = effectPool.obtain();
-                float[] color = fireworkColors[rng.nextInt(fireworkColors.length)];
-                effect.getEmitters().peek().getTint().setColors(color);
-                effect.setPosition(x, y);
-                effects.add(effect);
-
+                launcher.fire(new Vector2(x, y));
                 return true;
             }
         };
@@ -170,8 +122,6 @@ public class TestScreen implements Screen
     public void dispose()
     {
         // never called automatically
-        batch.dispose();
-        prototype.dispose();
         /* music.dispose(); */
     }
 }
