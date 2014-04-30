85836e0ae3fde9c2e83f9357f4fb5bb07aad02f2
MaryHal <maryhal@users.noreply.github.com>
2014-04-30 16:13:18 -0700

    A bit of work on FireworkGroup The code is quickly becoming more and more complicated.

--- core/src/fp/infiniteset/Burst/Fireworks/FireworkGroup.java
+++ core/src/fp/infiniteset/Burst/Fireworks/FireworkGroup.java
@@ -6,10 +6,27 @@ import java.util.ArrayList;
 
 public class FireworkGroup implements Pool.Poolable
 {
+    private int comboIndex;
     private ArrayList<Firework> comboList;
 
     public void reset()
     {
+        comboIndex = 0;
         comboList.clear();
     }
+
+    public void addToList(Firework f)
+    {
+        comboList.add(f);
+    }
+
+    public void clearList(FireworkPool fireworkPool)
+    {
+        for (int i = 0; i < comboList.size(); i++)
+        {
+            fireworkPool.free(comboList.get(i));
+        }
+        reset();
+    }
 }
+
